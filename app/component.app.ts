409c21ce497d27fcc674e810a2d34017cf8a020d
Jeremy Owens <jowens@shawinc.com>
2019-07-11 11:23:15 -0400

    first commit

---
+++ app/component.app.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app',
+    templateUrl: "./app/component.app.html",
+    styleUrls:['./app/component.app.css']
+})
+
+export class AppComponent{};
